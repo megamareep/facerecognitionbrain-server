6b429b277d93091a5935b9ef424d7fa7ebe2f777
megamareep <megamareep@gmail.com>
2020-05-13 22:12:20 +0100

    adding port env

--- server.js
+++ server.js
@@ -29,7 +29,7 @@ app.post('/imageurl', image.handleApiCall)
 
 
 
-app.listen(3000, () => {
-    console.log('app is running on port 3000')
+app.listen(process.env.PORT || 3000, () => {
+    console.log(`app is running on port 3000 ${process.env.PORT}`)
 });
 
